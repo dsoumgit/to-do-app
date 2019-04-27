e772c2de3d2ed56ad7ce468d024cfb05e900afcf
Dara Soumgit <dara.soumgit@gmail.com>
2019-04-27 08:40:59 -0400

    Updates

---
+++ precache-manifest.5d1f9eda6034367a033eaca14bc7cb59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6c331916fc71dd9ec96",
+    "url": "/to-do-app/static/css/main.1fe45f54.chunk.css"
+  },
+  {
+    "revision": "c6c331916fc71dd9ec96",
+    "url": "/to-do-app/static/js/main.513cf8b5.chunk.js"
+  },
+  {
+    "revision": "6cf4045563d5e41dd4b3",
+    "url": "/to-do-app/static/js/runtime~main.01361c46.js"
+  },
+  {
+    "revision": "b8b79a9c1cf70e84e11a",
+    "url": "/to-do-app/static/js/2.353a5b32.chunk.js"
+  },
+  {
+    "revision": "f2d794cb420b79fdf4755159427fe1cb",
+    "url": "/to-do-app/index.html"
+  }
+];
